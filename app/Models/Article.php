44560142e4ac4b8fcc913ed7239d5aefdace8ff3
Zaio Klepoyshkov <master@lisi4ok.com>
2025-09-04 17:34:16 +0300

    Add Article all

---
+++ app/Models/Article.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Article extends Model
+{
+    /** @use HasFactory<\Database\Factories\ArticleFactory> */
+    use HasFactory, HasSlug;
+
+    protected $table = 'articles';
+
+    protected $fillable = [
+        'title',
+        'url',
+        'image',
+        'source',
+        'author',
+        'slug',
+        'description',
+        'content',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime:Y-m-d H:i:s',
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug')
+            ->usingLanguage(config('app.locale'))
+            ->preventOverwrite();
+    }
+}
